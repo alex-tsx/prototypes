c209108a4feae3ba1d16beb0f8fac31c2cde554e
alex-s <alex.seleznov@gmail.com>
2020-07-14 13:21:45 +0300

    [FUNCTION] ++

--- types/function.js
+++ types/function.js
@@ -15,15 +15,43 @@ Object.defineProperty(Function.prototype, 'logger', {
         }.bind(this);
     }
 });
-Function.prototype.time = function() {
-    const start = new Date().getTime();
-    this(...arguments);
-    return new Date().getTime() - start;
-};
+Object.defineProperty(Function.prototype, 'time', {
+    value: function() {
+        const start = Date.now();
+        this(...arguments);
+        //
+        return Date.now() - start;
+    }
+});
+/* Avearge time of the function execution, based on runsN calls */
 Object.defineProperty(Function.prototype, 'atime', {
     value: function(runsN, ...args) {
-        const start = new Date().getTime();
+        const start = Date.now();
         for(let i = 0; i < runsN; i++) this(...args);
-        return ( new Date().getTime() - start ) / runsN;
+        //
+        return ( Date.now() - start ) / runsN;
+    }
+});
+/*
+* @param wait: :Number,
+* ? @param options: ? :{
+*   leading, maxWait, trailing
+* }
+* WARNING: Never used/called/tested
+*/
+Object.defineProperty(Function.prototype, 'debounce', {
+    value: function(wait=0) {
+        const thisFunction = this;
+        let timeoutId;
+        return function() {
+            const args = arguments;
+            if(timeoutId) { clearTimeout(timeoutId); timeoutId = null; }
+            if(wait > 0) timeoutId = setTimeout(() => {
+                timeoutId = null;
+                thisFunction(...args);
+            }, wait);
+            else if(wait === 0) thisFunction(...args);
+            else console.warn(`[Function.prototype.debounce] WTF?!`);
+        }
     }
 });
